19d50b89e885a21956cbfd80879974411339ad8a
Elvira <ajdar.elvira@hotmail.com>
2025-12-19 01:19:51 +0100

    small changes

--- main_module/main.py
+++ main_module/main.py
@@ -1,7 +1,7 @@
 from finance.analysis import FinanceAnalysis
 from system.loggin_util import setup_logger
 
-#FUnktion som säkerhetställer att användaren matar in heltal
+#Funktion som säkerhetställer att användaren matar in heltal
 def safe_int_input(text):
     while True:
         try:
@@ -17,18 +17,19 @@ def start_moneybot():
     #Frågar användaren om värden
     income = safe_int_input("Monthly income: ")
     rent = safe_int_input("Rent: ")
-    food = safe_input("Food costs: ")
+    food = safe_int_input("Food costs: ")
 
-    transactions = [income,-rent,-food]
+    transactions = [income, -rent, -food]
     analysis = FinanceAnalysis(transactions)
 
-    print("\n----ECONOMIC REPORT----")
+    print("\n====ECONOMIC REPORT====")
     print("Income:", analysis.total_income())
     print("Expenses:", analysis.total_expenses())
     print("Balance:", analysis.total_balance())
     print("Predicted next month:", analysis.predicted_next_month())
 
-
+if __name__ == "__main__":
+    start_moneybot()
 
 
 
